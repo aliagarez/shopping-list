5024b2f10f922c55e6a9ac2aa1298ff79d092660
alia.garcia <alia.garcia@iteso.mx>
2026-07-24 14:33:37 -0600

    Add products and display them in a list

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const [name, setName] = useState("")
+  const [products, setProducts] = useState([])
+
+function addProduct(event){
+  event.preventDefault();
+  setProducts(
+    [...products, name] // Create a new list copying the content of the original list and add the new product
+  );
+  setName('');
+}
 
   return (
     <>
@@ -12,14 +23,25 @@ function App() {
           </p>
         </div>
 
-        <form id="input">
-          <input type="text"/>
+        <form id="input" onSubmit={addProduct}>
+          <input 
+            type="text" 
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
           <button type="submit">Agregar</button>
         </form>
       </section>
 
       <section id="list">
-
+        <ul type="checkbox">
+          {
+            products.map((product) => { // For each product in products add a new li element:
+            return <li>{product}</li>
+            })
+          }
+        </ul>
       </section>
     </>
   )
